e5ba288908c97f1538321d8c25d54a5eb6bc7465
Lyanbin <champagne.lyb@gmail.com>
2019-01-04 17:25:26 +0800

    css ast

--- src/converStyle/index.js
+++ src/converStyle/index.js
@@ -6,17 +6,19 @@ const path = require('path');
 const util = require('../util.js');
 const config = require('../config.js');
 const less = require('less');
+const postcss = require('postcss');
+const Assets = require('assets');
 module.exports = function converJson(dir, aimType) {
     if (!aimType) {
         console.log('No aim type, do nothing...');
         return false;
     }
     console.log('Convering the style files...');
-    util.recursiveReadDir(dir, handleJson(aimType));
+    util.recursiveReadDir(dir, handleJson(aimType, dir));
 };
 
 
-function handleJson(aimType) {
+function handleJson(aimType, dir) {
     // 这里可以针对不同的aimType做处理
     const aimFileType = config[aimType].style;
     return async function (filePath) {
@@ -38,11 +40,52 @@ function handleJson(aimType) {
                 }
                 content = lessRes ? lessRes.css : content;
             }
-            content = content.replace(/\.(?:wxss|css|acss)/ig, `.${aimFileType}`);
+            // content = content.replace(/\.(?:wxss|css|acss)/ig, `.${aimFileType}`);
+
+            let astObj = postcss.parse(content);
+            let newAstObj = await handleCssAst(astObj ,aimFileType, filePath, dir);
+            let resContent = '';
+            postcss.stringify(newAstObj, (str) => {
+                resContent += str;
+            });
+            // 最后目标文件的路径
             let cssPath = filePath.replace(/.less$/, `.${aimFileType}`);
             fs.removeSync(filePath);
             fs.ensureFileSync(cssPath);
-            fs.writeFileSync(cssPath, content);
+            fs.writeFileSync(cssPath, resContent);
         }
+    };
+}
+
+async function handleCssAst(astObj, aimFileType, filePath, dir) {
+    let ast = astObj.nodes || [];
+    let resolver = new Assets();
+    await Promise.all(ast.map(async (item) => {
+        if (item.type === 'atrule' && item.name === 'import') {
+            item.params = item.params.replace(/\.(?:wxss|css|acss)/ig, `.${aimFileType}`);
+        } else if (item.type === 'rule') {
+            await Promise.all(item.nodes.map(async (declNode) => {
+                if (/^background(?:-image)?$/.test(declNode.prop) && /url\((['"]?)([^'"]*?)\1\)/.test(declNode.value)) {
+                    let originValue = declNode.value;
+                    let imagePath = originValue.match(/url\((['"]?)([^'"]*?)\1\)/)[2];
+                    let isNetSource = /^((?:http|https):)?\/\//.test(imagePath.trim());
+                    if (!isNetSource) {
+                        // 这里要base64
+                        let truePath = path.resolve(path.dirname(filePath), imagePath);
+                        if (/^\//.test(imagePath)) {
+                            truePath = path.resolve(`${dir}${imagePath}`);
+                        }
+                        let base64 = await resolver.data(truePath);
+                        declNode.value = `url(${base64})`;
+                        return declNode;
+                    }
+                }
+            }));
+        }
+        return item;
+    }));
+    return {
+        ...astObj,
+        nodes: ast
     };
 }
